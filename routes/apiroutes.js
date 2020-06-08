943e363803a45a15e7572c1c84271281268cc502
kmeersman <katie4682@yahoo.com>
2020-06-08 13:51:43 -0600

    Deleted unnecessary files and code

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
-// const db = require("../db/db.js");
 const { v4: uuidv4 } = require('uuid');;
 const filePath = path.join(__dirname, "../db/db.json");
+
 //GET /api/notes - Should read the db.json file and return all saved notes as JSON.
 router.get("/notes", function (req, res) {
   res.sendFile(filePath);
-  // db.getNote().then((note) => res.json(note));
 });
 
 //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
@@ -23,9 +22,6 @@ router.post("/notes", function (req, res) {
       res.json(true);
     });
   });
-  // db.addNote(req.body)
-  //     .then((note) => res.json(note))
-  //     .catch(err => res.status(500).json(err));
 });
 
 //DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. (UUID)
